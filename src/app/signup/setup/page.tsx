e950c6e0a8b0caf2c2bf4aec2490da3ac9515a53
Yuri Martins <develop@yumartins.com.br>
2023-11-12 19:00:43 -0300

    feat(setup): add more steps

--- src/app/signup/setup/page.tsx
+++ src/app/signup/setup/page.tsx
@@ -21,13 +21,25 @@ const first = z.object({
   username: z.string().min(1, "Enter your user name"),
 })
 
+const second = z.object({
+  company: z.string().min(1, "Enter your company name"),
+  industry: z.string().min(1, "Enter the industry"),
+  headCount: z.string().min(1, "Enter the head count"),
+  companyAddress: z.string().min(1, "Enter the company address"),
+})
+
+const third = z.object({})
+
 const schema = {
   1: first,
+  2: second,
+  3: third,
 }
 
 type FirstFormSchema = z.infer<typeof first>
+type SecondFormSchema = z.infer<typeof second>
 
-type FormSchema = FirstFormSchema
+type FormSchema = FirstFormSchema & SecondFormSchema
 
 export default function Setup() {
   const [step, setStep] = useState(1)
@@ -36,10 +48,14 @@ export default function Setup() {
     resolver: zodResolver(schema[step as keyof typeof schema]),
   })
 
-  async function submit(data: FormSchema) {
+  async function submit() {
+    const data = methods.getValues()
+
+    if (step <= 2) return setStep((prev) => prev + 1)
+
     console.log({ data })
 
-    setStep((prev) => prev + 1)
+    // Send request to backend.
   }
 
   return (
@@ -49,36 +65,73 @@ export default function Setup() {
       <div className="flex w-full mt-5 py-14 px-20 flex-col border border-solid border-gray-300 rounded-xl">
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(submit)}>
-            <FormUpload name="avatar" className="mx-auto" />
+            <div className={cn("flex-col", step === 1 ? "flex" : "hidden")}>
+              <FormUpload name="avatar" className="mx-auto" />
 
-            <FormInput
-              name="username"
-              label="User Name"
-              className="mt-7 mb-6"
-              placeholder="Enter your user name"
-            />
+              <FormInput
+                name="username"
+                label="User Name"
+                className="mt-7 mb-6"
+                placeholder="Enter your user name"
+              />
 
-            <FormInput
-              name="name"
-              label="Full Name"
-              placeholder="Enter your full name"
-            />
+              <FormInput
+                name="name"
+                label="Full Name"
+                placeholder="Enter your full name"
+              />
 
-            <FormPhoneInput
-              name="phone"
-              label="Phone Number"
-              className="my-6"
-              placeholder="Enter your phone number"
-            />
+              <FormPhoneInput
+                name="phone"
+                label="Phone Number"
+                className="my-6"
+                placeholder="Enter your phone number"
+              />
 
-            <FormInput
-              name="country"
-              label="Country"
-              placeholder="Enter your country"
-            />
+              <FormInput
+                name="country"
+                label="Country"
+                placeholder="Enter your country"
+              />
+            </div>
+
+            <div
+              className={cn("flex-col gap-6", step === 2 ? "flex" : "hidden")}
+            >
+              <FormInput
+                name="company"
+                label="Company Name"
+                placeholder="Enter your company name"
+              />
+
+              <FormInput
+                name="industry"
+                label="Industry"
+                placeholder="Enter the industry"
+              />
+
+              <FormInput
+                name="headCount"
+                label="Head Count"
+                placeholder="Enter the head count"
+              />
+
+              <FormInput
+                name="companyAddress"
+                label="Company Address (Optional)"
+                placeholder="Enter the company address"
+              />
+            </div>
+
+            <div className={cn(step === 3 ? "flex" : "hidden")}>
+              <p className="text-gray-800">
+                I agree to the Terms of Service and Privacy Policy of the
+                Realvault platform to start using it.
+              </p>
+            </div>
 
             <Button type="submit" className="mt-12 w-full">
-              Next
+              {step === 3 ? "Confirm" : "Next"}
             </Button>
           </form>
         </FormProvider>
