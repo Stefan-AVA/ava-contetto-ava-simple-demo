aef5dae59755101552a545f35386752f7be9f11d
Yuri Martins <develop@yumartins.com.br>
2023-11-12 16:28:51 -0300

    feat(components): upload

--- src/app/signup/setup/page.tsx
+++ src/app/signup/setup/page.tsx
@@ -2,17 +2,48 @@
 
 import { useState } from "react"
 import { cn } from "@/utils/classname"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { FormProvider, useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { FormUpload } from "@/components/upload"
 
 import Steps from "./steps"
 
+const first = z.object({
+  avatar: z.custom<FileList>(),
+})
+
+const schema = {
+  1: first,
+}
+
+type FirstFormSchema = z.infer<typeof first>
+
+type FormSchema = FirstFormSchema
+
 export default function Setup() {
   const [step, setStep] = useState(1)
+
+  const methods = useForm<FormSchema>({
+    resolver: zodResolver(schema[step as keyof typeof schema]),
+  })
+
+  async function submit(data: FormSchema) {
+    console.log({ data })
+  }
 
   return (
     <div className="flex flex-col items-center mt-8 w-full">
       <Steps step={step} />
 
-      <div className="flex w-full mt-5 py-14 px-20 flex-col border border-solid border-r-gray-300 rounded-xl"></div>
+      <div className="flex w-full mt-5 py-14 px-20 flex-col border border-solid border-r-gray-300 rounded-xl">
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(submit)}>
+            <FormUpload name="avatar" />
+          </form>
+        </FormProvider>
+      </div>
     </div>
   )
 }
---
+++ src/components/upload.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import {
+  forwardRef,
+  useState,
+  type ChangeEvent,
+  type ComponentProps,
+} from "react"
+import Image from "next/image"
+import { cn } from "@/utils/classname"
+import { Plus, UserCircle2 } from "lucide-react"
+import { useFormContext } from "react-hook-form"
+
+interface UploadProps extends ComponentProps<"input"> {
+  error?: string
+}
+
+interface FormUploadProps extends UploadProps {
+  name: string
+}
+
+export const Upload = forwardRef<HTMLInputElement, UploadProps>(
+  ({ error, className, ...rest }, ref) => {
+    const [preview, setPreview] = useState<string | null>(null)
+
+    function onChange(e: ChangeEvent<HTMLInputElement>) {
+      const file = e.target.files?.[0]
+
+      if (!file) {
+        setPreview(null)
+
+        return
+      }
+
+      if (rest.onChange) rest.onChange(e)
+
+      setPreview(URL.createObjectURL(file))
+    }
+
+    return (
+      <div
+        className={cn(
+          "flex py-7 px-9 w-full flex-col cursor-pointer relative border border-dashed border-gray-300 rounded",
+          className
+        )}
+      >
+        <div className="flex flex-col items-center justify-center pointer-events-none bg-gray-300 w-28 h-28 rounded-full aspect-square relative">
+          {preview && (
+            <Image
+              src={preview}
+              alt=""
+              fill
+              className="w-full h-full object-cover rounded-full pointer-events-none select-none"
+            />
+          )}
+
+          {!preview && (
+            <UserCircle2 size={64} strokeWidth={1.5} className="text-white" />
+          )}
+
+          <div className="flex items-center justify-center absolute bottom-0 right-0 bg-gray-300 text-white rounded-full w-7 h-7 border border-solid border-white">
+            <Plus size={20} />
+          </div>
+        </div>
+
+        <input
+          ref={ref}
+          {...rest}
+          type="file"
+          onChange={onChange}
+          className="top-0 left-0 w-full h-full absolute opacity-0 cursor-pointer"
+        />
+
+        {error && <p className="text-sm mt-1 text-red-500">{error}</p>}
+      </div>
+    )
+  }
+)
+
+Upload.displayName = "Upload"
+
+export function FormUpload({ name, ...rest }: FormUploadProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext()
+
+  return (
+    <Upload
+      {...rest}
+      {...register(name)}
+      error={errors[name]?.message as string}
+    />
+  )
+}
