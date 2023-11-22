086422c48475fd1833840e9b99ffb91cd00465e3
Yuri Martins <develop@yumartins.com.br>
2023-11-22 07:55:59 -0300

    refactor(sidebar): migrate to components

--- src/app/app/agent-orgs/[agentId]/layout.tsx
+++ src/app/app/agent-orgs/[agentId]/layout.tsx
@@ -1,16 +1,72 @@
-import { type PropsWithChildren } from "react"
-import { Box, Stack } from "@mui/material"
+"use client"
 
-import Sidebar from "./sidebar"
+import { useMemo, type PropsWithChildren } from "react"
+import { useParams, usePathname } from "next/navigation"
+import { RootState } from "@/redux/store"
+import { Box, Stack } from "@mui/material"
+import {
+  Contact,
+  LayoutDashboardIcon,
+  Search,
+  SettingsIcon,
+} from "lucide-react"
+import { useSelector } from "react-redux"
+
+import { AgentRole } from "@/types/agentProfile.types"
+import Sidebar from "@/components/sidebar"
 
 export default function Layout({ children }: PropsWithChildren) {
+  const pathName = usePathname()
+  const { agentId } = useParams()
+
+  const agentOrgs = useSelector((state: RootState) => state.app.agentOrgs)
+
+  const role = useMemo(
+    () => agentOrgs.find((agent) => agent._id === agentId)?.role,
+    [agentId, agentOrgs]
+  )
+
+  const routes = useMemo(
+    () => [
+      {
+        path: `/app/agent-orgs/${agentId}`,
+        icon: <LayoutDashboardIcon />,
+        label: "Search",
+        active: pathName === `/app/agent-orgs/${agentId}`,
+      },
+      {
+        path: `/app/agent-orgs/${agentId}/contacts`,
+        icon: <Contact />,
+        label: "Contacts",
+        active: pathName.includes("contacts"),
+      },
+      {
+        path: `/app/agent-orgs/${agentId}/search-results`,
+        icon: <Search />,
+        label: "My Searches",
+        active: pathName.includes("search-results"),
+      },
+      ...(role === AgentRole.owner || role === AgentRole.admin
+        ? [
+            {
+              path: `/app/agent-orgs/${agentId}/settings`,
+              icon: <SettingsIcon />,
+              label: "Settings",
+              active: pathName.includes("settings"),
+            },
+          ]
+        : []),
+    ],
+    [pathName, agentId, role]
+  )
+
   return (
     <Stack
       padding={{ xs: 1, md: 0 }}
       spacing={{ xs: 2, md: 0 }}
       direction={{ xs: "column", md: "row" }}
     >
-      <Sidebar />
+      <Sidebar routes={routes} />
 
       <Box sx={{ p: 5 }}>{children}</Box>
     </Stack>
--- src/app/app/contact-orgs/[contactId]/layout.tsx
+++ src/app/app/contact-orgs/[contactId]/layout.tsx
@@ -1,12 +1,37 @@
-import { type PropsWithChildren } from "react"
-import { Stack } from "@mui/material"
+"use client"
 
-import Sidebar from "./sidebar"
+import { useMemo, type PropsWithChildren } from "react"
+import { useParams, usePathname } from "next/navigation"
+import { Stack } from "@mui/material"
+import { LayoutDashboardIcon, Search } from "lucide-react"
+
+import Sidebar from "@/components/sidebar"
 
 export default function Layout({ children }: PropsWithChildren) {
+  const pathName = usePathname()
+  const { contactId } = useParams()
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Search",
+        path: `/app/agent-orgs/${contactId}`,
+        icon: <LayoutDashboardIcon />,
+        active: pathName === `/app/agent-orgs/${contactId}`,
+      },
+      {
+        label: "My Searches",
+        path: `/app/agent-orgs/${contactId}/search-results`,
+        icon: <Search />,
+        active: pathName.includes("search-results"),
+      },
+    ],
+    [pathName, contactId]
+  )
+
   return (
     <Stack direction="row">
-      <Sidebar />
+      <Sidebar routes={routes} />
 
       {children}
     </Stack>
