54f8b2c54ba3d5ae00595f8040205ab3afd04f8b
pyon123 <pyon123@outlook.com>
2023-11-24 09:25:54 -0500

    add sidebar mobile view

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,10 @@
-import { type JSX } from "react"
+import { useState, type JSX } from "react"
 import { Route } from "next"
 import Link from "next/link"
-import { Stack, Typography } from "@mui/material"
+import { Card, Stack, Typography } from "@mui/material"
+import { ListFilter } from "lucide-react"
+
+import Dropdown from "./drop-down"
 
 type Router = {
   icon: JSX.Element
@@ -15,6 +18,7 @@ interface SidebarProps {
 }
 
 export default function Sidebar({ routes }: SidebarProps) {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <Stack
@@ -57,6 +61,65 @@ export default function Sidebar({ routes }: SidebarProps) {
           </Stack>
         ))}
       </Stack>
+      <Dropdown
+        open={open}
+        onClose={() => setOpen(false)}
+        ancher={
+          <Stack
+            padding={1.5}
+            display={{ xs: "flex", md: "none" }}
+            alignItems="center"
+            justifyContent="space-between"
+            direction="row"
+            onClick={() => setOpen(true)}
+            sx={{
+              borderBottom: "1px solid",
+              borderBottomColor: "gray.300",
+              bgcolor: "gray.200",
+            }}
+          >
+            <Stack direction="row" alignItems="center" spacing={2}>
+              {routes.find((route) => route.active)?.icon}
+              <Typography variant="h5" fontWeight={700}>
+                {routes.find((route) => route.active)?.label}
+              </Typography>
+            </Stack>
+            <ListFilter />
+          </Stack>
+        }
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <Card sx={{ padding: 2, width: "calc(100vw - 20px)" }}>
+          {routes.map(({ label, path, icon, active }) => (
+            <Stack
+              sx={{
+                p: 1,
+                mb: 1,
+                gap: 1.5,
+                color: "gray.800",
+                bgcolor: active ? "gray.200" : "white",
+                alignItems: "center",
+                borderRadius: 1.5,
+              }}
+              key={path}
+              href={path as Route}
+              component={Link}
+              direction="row"
+              onClick={() => setOpen(false)}
+            >
+              {icon}
+              <Typography sx={{ fontWeight: 500 }}>{label}</Typography>
+            </Stack>
+          ))}
+        </Card>
+      </Dropdown>
     </>
   )
 }
