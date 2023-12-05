e90cc14c5e7c0897185ee362a4da2b887896fae0
pyon123 <pyon123@outlook.com>
2023-12-05 12:42:08 -0500

    update next config for remote image pattern

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,7 @@ const config = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "dvvjkgh94f2v6.cloudfront.net",
-      },
-      {
-        protocol: "https",
-        hostname: "ddfcdn.realtor.ca",
-      },
-      {
-        protocol: "https",
-        hostname: "ava-org-logos.s3.ca-central-1.amazonaws.com",
+        hostname: "**",
       },
     ],
   },
