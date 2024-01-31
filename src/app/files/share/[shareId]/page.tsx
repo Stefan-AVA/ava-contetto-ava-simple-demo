53ca37888da8ff4baaab7b434a10c7c32fe8cc4d
Yuri Martins <develop@yumartins.com.br>
2024-01-31 07:46:11 -0300

    feat(share): add preview shared screen

--- src/app/files/share/[shareId]/page.tsx
+++ src/app/files/share/[shareId]/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+// import Image from "next/image"
 import {
   useCopySharedFileMutation,
   useGetSharedFileQuery,
   useLazyGetSharedFileQuery,
 } from "@/redux/apis/fileshare"
+import { Stack, Typography } from "@mui/material"
+import { DownloadCloud, FolderPlus } from "lucide-react"
+
+import Loading from "@/components/Loading"
 
 type PageProps = {
   params: {
@@ -31,7 +36,115 @@ const Page = ({ params, searchParams }: PageProps) => {
 
   if (!orgId || !code) return <div>No Content</div>
 
-  return <div>Loading...</div>
+  if (isLoading) return <Loading />
+
+  return (
+    <Stack
+      sx={{
+        p: 4,
+        top: 0,
+        left: 0,
+        color: "white",
+        width: "100svw",
+        zIndex: 999,
+        position: "fixed",
+        minHeight: "100svh",
+        backgroundColor: "rgba(0, 0, 0, .8)",
+      }}
+    >
+      <Stack
+        sx={{
+          gap: 3,
+          position: "relative",
+          alignItems: {
+            sm: "center",
+          },
+          flexDirection: {
+            xs: "column",
+            sm: "row",
+          },
+          justifyContent: {
+            xs: "space-between",
+            md: "flex-end",
+          },
+        }}
+      >
+        <Typography
+          sx={{
+            left: 0,
+            right: 0,
+            position: { md: "absolute" },
+            textAlign: { xs: "left", md: "center" },
+            fontWeight: "700",
+          }}
+          variant="h6"
+        >
+          File Name (View Only)
+        </Typography>
+
+        <Stack
+          sx={{
+            gap: 6,
+            alignItems: "center",
+            flexDirection: "row",
+          }}
+        >
+          <Typography
+            sx={{
+              gap: 2,
+              color: "white",
+              display: "flex",
+              opacity: 0.7,
+              transition: "all .3s ease-in-out",
+              alignItems: "center",
+
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+            component="button"
+          >
+            <DownloadCloud />
+            Download
+          </Typography>
+
+          <Typography
+            sx={{
+              gap: 2,
+              color: "white",
+              display: "flex",
+              opacity: 0.7,
+              transition: "all .3s ease-in-out",
+              alignItems: "center",
+
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+            component="button"
+          >
+            <FolderPlus />
+            Save
+          </Typography>
+        </Stack>
+      </Stack>
+
+      <Typography
+        sx={{ m: "auto", textAlign: "center", maxWidth: "20rem" }}
+        variant="h4"
+      >
+        This file can not be opened. Download to open.
+      </Typography>
+
+      {/* <Image
+        src="/assets/signup-background.jpg"
+        alt=""
+        width={960}
+        style={{ margin: "auto" }}
+        height={480}
+      /> */}
+    </Stack>
+  )
 }
 
 export default Page
