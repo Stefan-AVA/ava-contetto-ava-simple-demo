3ba43d0e8a8564f4cc7f87d77a5eae842ff77306
Yuri Martins <develop@yumartins.com.br>
2024-02-08 07:03:43 -0300

    refactor(templates): init new design

--- src/app/app/agent-orgs/[agentId]/marketing-hub/(hub)/collections/page.tsx
+++ src/app/app/agent-orgs/[agentId]/marketing-hub/(hub)/collections/page.tsx
@@ -1,5 +1,43 @@
-import { Typography } from "@mui/material"
+"use client"
 
-export default function Collections() {
-  return <Typography>Collections</Typography>
+import { useMemo } from "react"
+import { useGetTemplatesQuery } from "@/redux/apis/templates"
+import { RootState } from "@/redux/store"
+import { Stack, Typography } from "@mui/material"
+import { useSelector } from "react-redux"
+
+import Loading from "@/components/Loading"
+
+interface PageProps {
+  params: {
+    agentId: string
+  }
+}
+
+export default function Collections({ params }: PageProps) {
+  const agentOrgs = useSelector((state: RootState) => state.app.agentOrgs)
+
+  const currentOrg = useMemo(
+    () => agentOrgs.find((agent) => agent._id === params.agentId)!,
+    [params.agentId, agentOrgs]
+  )
+
+  const { data, isLoading } = useGetTemplatesQuery(
+    {
+      orgId: currentOrg.orgId,
+    },
+    {
+      skip: !currentOrg,
+    }
+  )
+
+  console.log({ data })
+
+  if (isLoading) return <Loading />
+
+  return (
+    <Stack>
+      <Typography>Collections</Typography>
+    </Stack>
+  )
 }
